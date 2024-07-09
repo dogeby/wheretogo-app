371c8caa631607e5d0771a81acb25a589f1f3ce9
dogeby <dogeby@gmail.com>
2024-07-09 23:31:40 +0900

    ReviewContent 구현
    #41

---
+++ feature/contentdetail/src/main/java/com/dogeby/wheretogo/feature/contentdetail/model/RatingFilterOption.kt
@@ -0,0 +1,10 @@
+package com.dogeby.wheretogo.feature.contentdetail.model
+
+enum class RatingFilterOption(val rating: Int) {
+    ALL(0),
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+}
