552ad9c5c825ab5da9610a8c67b59c039aaf5b85
dogeby <dogeby@gmail.com>
2024-07-09 22:19:42 +0900

    ListTitleRow parameter 수정
    #45

--- core/ui/src/main/java/com/dogeby/wheretogo/core/ui/components/common/ListTitleRow.kt
+++ core/ui/src/main/java/com/dogeby/wheretogo/core/ui/components/common/ListTitleRow.kt
@@ -6,11 +6,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemColors
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,12 +22,18 @@ fun ListTitleRow(
     title: String,
     modifier: Modifier = Modifier,
     onClick: (() -> Unit)? = null,
-    trailingIcon: ImageVector = Icons.AutoMirrored.Filled.ArrowForward,
+    trailingIcon: ImageVector? = null,
     contentDescription: String? = null,
+    colors: ListItemColors = ListItemDefaults.colors(),
+    tonalElevation: Dp = ListItemDefaults.Elevation,
+    shadowElevation: Dp = ListItemDefaults.Elevation,
 ) {
     ListItem(
         headlineContent = {
-            Text(text = title)
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+            )
         },
         modifier = if (onClick != null) {
             modifier.clickable { onClick() }
@@ -31,13 +41,16 @@ fun ListTitleRow(
             modifier
         },
         trailingContent = {
-            onClick?.let {
+            if (trailingIcon != null && onClick != null) {
                 Icon(
                     imageVector = trailingIcon,
                     contentDescription = contentDescription,
                 )
             }
         },
+        colors = colors,
+        tonalElevation = tonalElevation,
+        shadowElevation = shadowElevation,
     )
 }
 
@@ -47,7 +60,6 @@ private fun ListTitleRowPreview_OnClickIsNull() {
     ListTitleRow(
         title = "Title",
         modifier = Modifier.padding(16.dp),
-        onClick = null,
     )
 }
 
@@ -58,5 +70,6 @@ private fun ListTitleRowPreview() {
         title = "Title",
         modifier = Modifier.padding(16.dp),
         onClick = {},
+        trailingIcon = Icons.AutoMirrored.Filled.ArrowForward,
     )
 }
