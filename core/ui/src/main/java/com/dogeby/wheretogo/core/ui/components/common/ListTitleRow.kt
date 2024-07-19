fb2a91a22e3844198767f7e9391825ceb10dc45a
dogeby <dogeby@gmail.com>
2024-07-19 16:12:03 +0900

    ListTitleRow에 ripple 효과 추가
    #67

--- core/ui/src/main/java/com/dogeby/wheretogo/core/ui/components/common/ListTitleRow.kt
+++ core/ui/src/main/java/com/dogeby/wheretogo/core/ui/components/common/ListTitleRow.kt
@@ -1,9 +1,11 @@
 package com.dogeby.wheretogo.core.ui.components.common
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemColors
@@ -11,6 +13,7 @@ import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,7 +39,10 @@ fun ListTitleRow(
             )
         },
         modifier = if (onClick != null) {
-            modifier.clickable { onClick() }
+            modifier.clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(),
+            ) { onClick() }
         } else {
             modifier
         },
