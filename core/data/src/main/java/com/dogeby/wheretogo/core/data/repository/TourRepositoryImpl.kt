ce1068301785d21a2ec21d70335a8ff11456606e
dogeby <dogeby@gmail.com>
2024-07-26 13:41:07 +0900

    TourRepository 구현
    - getPagedTourInfoByRegion 함수 추가
    
    #71

---
+++ core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepositoryImpl.kt
@@ -0,0 +1,50 @@
+package com.dogeby.wheretogo.core.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.dogeby.wheretogo.core.data.model.TourContentData
+import com.dogeby.wheretogo.core.data.paging.TourInfoByRegionPagingSource
+import com.dogeby.wheretogo.core.model.tour.ArrangeOption
+import com.dogeby.wheretogo.core.network.TourNetworkDataSource
+import com.dogeby.wheretogo.core.network.model.tour.TourInfoByRegionRequestBody
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlinx.coroutines.flow.Flow
+
+@Singleton
+class TourRepositoryImpl @Inject constructor(
+    private val tourNetworkDataSource: TourNetworkDataSource,
+) : TourRepository {
+
+    override fun getPagedTourInfoByRegion(
+        currentPage: Int,
+        numberOfRows: Int,
+        contentTypeId: String,
+        areaCode: String,
+        sigunguCode: String,
+        category1: String,
+        category2: String,
+        category3: String,
+        arrangeOption: ArrangeOption,
+    ): Flow<PagingData<TourContentData>> {
+        return Pager(
+            PagingConfig(numberOfRows),
+        ) {
+            TourInfoByRegionPagingSource(
+                tourNetworkDataSource = tourNetworkDataSource,
+                tourInfoByRegionRequestBody = TourInfoByRegionRequestBody(
+                    currentPage = currentPage,
+                    numberOfRows = numberOfRows,
+                    contentTypeId = contentTypeId,
+                    areaCode = areaCode,
+                    sigunguCode = sigunguCode,
+                    category1 = category1,
+                    category2 = category2,
+                    category3 = category3,
+                ),
+                arrangeOption = arrangeOption,
+            )
+        }.flow
+    }
+}
