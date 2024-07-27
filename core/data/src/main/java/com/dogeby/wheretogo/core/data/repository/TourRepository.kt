1fab6ee37026e81a6fb676d30c857cf1fea78186
dogeby <dogeby@gmail.com>
2024-07-28 05:02:42 +0900

    TourRepository에 페이지된 행사정보 조회 함수 추가
    #73

--- core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepository.kt
+++ core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepository.kt
@@ -1,9 +1,12 @@
 package com.dogeby.wheretogo.core.data.repository
 
 import androidx.paging.PagingData
+import com.dogeby.wheretogo.core.data.model.tour.festival.FestivalData
 import com.dogeby.wheretogo.core.data.model.tour.tourcontent.TourContentData
 import com.dogeby.wheretogo.core.model.tour.ArrangeOption
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 
 interface TourRepository {
 
@@ -16,6 +19,13 @@ interface TourRepository {
         category1: String = "",
         category2: String = "",
         category3: String = "",
-        arrangeOption: ArrangeOption = ArrangeOption.TITLE,
+        arrangeOption: ArrangeOption = ArrangeOption.MODIFIED_TIME,
     ): Flow<PagingData<TourContentData>>
+
+    fun getPagedFestivalInfo(
+        eventStartDate: Instant = Clock.System.now(),
+        currentPage: Int = 1,
+        numberOfRows: Int = 12,
+        arrangeOption: ArrangeOption = ArrangeOption.MODIFIED_TIME,
+    ): Flow<PagingData<FestivalData>>
 }
--- core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepositoryImpl.kt
+++ core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepositoryImpl.kt
@@ -3,14 +3,18 @@ package com.dogeby.wheretogo.core.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import com.dogeby.wheretogo.core.data.model.tour.festival.FestivalData
 import com.dogeby.wheretogo.core.data.model.tour.tourcontent.TourContentData
+import com.dogeby.wheretogo.core.data.paging.FestivalInfoPagingSource
 import com.dogeby.wheretogo.core.data.paging.TourInfoByRegionPagingSource
 import com.dogeby.wheretogo.core.model.tour.ArrangeOption
 import com.dogeby.wheretogo.core.network.TourNetworkDataSource
+import com.dogeby.wheretogo.core.network.model.tour.FestivalInfoRequestBody
 import com.dogeby.wheretogo.core.network.model.tour.TourInfoByRegionRequestBody
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
 
 @Singleton
 class TourRepositoryImpl @Inject constructor(
@@ -42,6 +46,27 @@ class TourRepositoryImpl @Inject constructor(
                     category1 = category1,
                     category2 = category2,
                     category3 = category3,
+                ),
+                arrangeOption = arrangeOption,
+            )
+        }.flow
+    }
+
+    override fun getPagedFestivalInfo(
+        eventStartDate: Instant,
+        currentPage: Int,
+        numberOfRows: Int,
+        arrangeOption: ArrangeOption,
+    ): Flow<PagingData<FestivalData>> {
+        return Pager(
+            PagingConfig(numberOfRows),
+        ) {
+            FestivalInfoPagingSource(
+                tourNetworkDataSource = tourNetworkDataSource,
+                festivalInfoRequestBody = FestivalInfoRequestBody(
+                    eventStartDate = eventStartDate,
+                    currentPage = currentPage,
+                    numberOfRows = numberOfRows,
                 ),
                 arrangeOption = arrangeOption,
             )
