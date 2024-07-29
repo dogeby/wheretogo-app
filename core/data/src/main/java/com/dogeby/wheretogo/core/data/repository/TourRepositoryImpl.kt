b49c0d383b404a1bb56012cab4e53781d0325e2d
dogeby <dogeby@gmail.com>
2024-07-29 10:22:39 +0900

    TourRepository에 키워드 검색 기능 추가
    #72

--- core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepositoryImpl.kt
+++ core/data/src/main/java/com/dogeby/wheretogo/core/data/repository/TourRepositoryImpl.kt
@@ -4,12 +4,15 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.dogeby.wheretogo.core.data.model.tour.FestivalData
+import com.dogeby.wheretogo.core.data.model.tour.KeywordSearchData
 import com.dogeby.wheretogo.core.data.model.tour.TourContentData
 import com.dogeby.wheretogo.core.data.paging.FestivalInfoPagingSource
+import com.dogeby.wheretogo.core.data.paging.KeywordSearchPagingSource
 import com.dogeby.wheretogo.core.data.paging.TourInfoByRegionPagingSource
 import com.dogeby.wheretogo.core.model.tour.ArrangeOption
 import com.dogeby.wheretogo.core.network.TourNetworkDataSource
 import com.dogeby.wheretogo.core.network.model.tour.FestivalInfoRequestBody
+import com.dogeby.wheretogo.core.network.model.tour.KeywordSearchRequestBody
 import com.dogeby.wheretogo.core.network.model.tour.TourInfoByRegionRequestBody
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -67,6 +70,39 @@ class TourRepositoryImpl @Inject constructor(
                     eventStartDate = eventStartDate,
                     currentPage = currentPage,
                     numberOfRows = numberOfRows,
+                ),
+                arrangeOption = arrangeOption,
+            )
+        }.flow
+    }
+
+    override fun searchKeyword(
+        keyword: String,
+        currentPage: Int,
+        numberOfRows: Int,
+        contentTypeId: String,
+        areaCode: String,
+        sigunguCode: String,
+        category1: String,
+        category2: String,
+        category3: String,
+        arrangeOption: ArrangeOption,
+    ): Flow<PagingData<KeywordSearchData>> {
+        return Pager(
+            PagingConfig(numberOfRows),
+        ) {
+            KeywordSearchPagingSource(
+                tourNetworkDataSource = tourNetworkDataSource,
+                keywordSearchRequestBody = KeywordSearchRequestBody(
+                    keyword = keyword,
+                    currentPage = currentPage,
+                    numberOfRows = numberOfRows,
+                    contentTypeId = contentTypeId,
+                    areaCode = areaCode,
+                    sigunguCode = sigunguCode,
+                    category1 = category1,
+                    category2 = category2,
+                    category3 = category3,
                 ),
                 arrangeOption = arrangeOption,
             )
