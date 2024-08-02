a97a4e39fcc73b29b0d25b8ac61ef5fe9eef00ef
dogeby <dogeby@gmail.com>
2024-08-02 12:41:57 +0900

    지역정보 목록 조회 UseCase 구현
    #97

---
+++ core/domain/src/main/java/com/dogeby/wheretogo/core/domain/model/tour/LocationInfo.kt
@@ -0,0 +1,13 @@
+package com.dogeby.wheretogo.core.domain.model.tour
+
+import com.dogeby.wheretogo.core.data.model.tour.LocationInfoData
+
+data class LocationInfo(
+    val code: String,
+    val name: String,
+)
+
+internal fun LocationInfoData.toLocationInfo() = LocationInfo(
+    code = code,
+    name = name,
+)
