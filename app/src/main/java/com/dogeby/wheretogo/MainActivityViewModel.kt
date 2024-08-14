398cdfa6469c132a54cbfc954e1c3012a5f87a9a
dogeby <dogeby@gmail.com>
2024-08-14 23:59:37 +0900

    MainActivityViewModel에 AreaInfo fetch 추가
    #133

--- app/src/main/java/com/dogeby/wheretogo/MainActivityViewModel.kt
+++ app/src/main/java/com/dogeby/wheretogo/MainActivityViewModel.kt
@@ -3,21 +3,29 @@ package com.dogeby.wheretogo
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dogeby.wheretogo.core.domain.tour.FetchContentTypeInfoMapUseCase
+import com.dogeby.wheretogo.core.domain.tour.areainfo.FetchAreaInfoMapUseCase
 import com.dogeby.wheretogo.model.MainActivityUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(
+    fetchAreaInfoMapUseCase: FetchAreaInfoMapUseCase,
     fetchContentTypeInfoMapUseCase: FetchContentTypeInfoMapUseCase,
 ) : ViewModel() {
 
     val uiState = fetchContentTypeInfoMapUseCase()
-        .map {
-            if (it.isSuccess) MainActivityUiState.Success else MainActivityUiState.Failure
+        .combine(
+            fetchAreaInfoMapUseCase(),
+        ) { contentTypeInfoResult: Result<Unit>, areaInfoResult: Result<Unit> ->
+            if (contentTypeInfoResult.isSuccess && areaInfoResult.isSuccess) {
+                MainActivityUiState.Success
+            } else {
+                MainActivityUiState.Failure
+            }
         }
         .stateIn(
             scope = viewModelScope,
