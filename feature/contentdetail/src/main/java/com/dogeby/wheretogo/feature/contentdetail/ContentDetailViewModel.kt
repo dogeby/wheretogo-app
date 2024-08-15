14f81ca1195156d98e2a1bfb9548dbc50a370291
dogeby <dogeby@gmail.com>
2024-08-15 19:15:38 +0900

    ContentDetailViewModel 구현
    #141

---
+++ feature/contentdetail/src/main/java/com/dogeby/wheretogo/feature/contentdetail/ContentDetailViewModel.kt
@@ -0,0 +1,55 @@
+package com.dogeby.wheretogo.feature.contentdetail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dogeby.wheretogo.core.common.decoder.StringDecoder
+import com.dogeby.wheretogo.core.domain.tour.GetCommonInfoUseCase
+import com.dogeby.wheretogo.core.ui.model.ReviewWithWriterListUiState
+import com.dogeby.wheretogo.feature.contentdetail.model.ContentDetailScreenUiState
+import com.dogeby.wheretogo.feature.contentdetail.model.RatingFilterOption
+import com.dogeby.wheretogo.feature.contentdetail.navigation.ContentDetailArgs
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+@HiltViewModel
+class ContentDetailViewModel @Inject constructor(
+    stringDecoder: StringDecoder,
+    savedStateHandle: SavedStateHandle,
+    getCommonInfoUseCase: GetCommonInfoUseCase,
+) : ViewModel() {
+
+    private val contentDetailArgs = ContentDetailArgs(savedStateHandle, stringDecoder)
+
+    val contentDetailScreenUiState = getCommonInfoUseCase(contentDetailArgs.contentId)
+        .map { result ->
+            val commonInfo = result.getOrNull()
+            commonInfo?.let {
+                ContentDetailScreenUiState.Success(
+                    id = it.contentId,
+                    title = it.title,
+                    avgStarRating = 0.0,
+                    modifiedTime = it.modifiedTime,
+                    reviewWithWriterListUiState = ReviewWithWriterListUiState.Loading,
+                    imgSrcs = listOf(it.firstImageSrc ?: ""),
+                    categories = listOf(
+                        it.majorCategoryInfo?.name ?: "",
+                        it.mediumCategoryInfo?.name ?: "",
+                        it.minorCategoryInfo?.name ?: "",
+                    ),
+                    overview = it.overview ?: "",
+                    tel = it.phoneNumber ?: "",
+                    homepage = it.homepage ?: "",
+                    ratingFilterOption = RatingFilterOption.ALL,
+                )
+            } ?: ContentDetailScreenUiState.Loading
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = ContentDetailScreenUiState.Loading,
+        )
+}
--- feature/contentdetail/src/main/java/com/dogeby/wheretogo/feature/contentdetail/model/ContentDetailScreenUiState.kt
+++ feature/contentdetail/src/main/java/com/dogeby/wheretogo/feature/contentdetail/model/ContentDetailScreenUiState.kt
@@ -8,15 +8,15 @@ sealed interface ContentDetailScreenUiState {
 
     data class Success(
         val id: String,
-        val imgSrcs: List<Any>,
         val title: String,
         val avgStarRating: Double,
         val modifiedTime: String,
+        val reviewWithWriterListUiState: ReviewWithWriterListUiState,
+        val imgSrcs: List<Any> = emptyList(),
         val categories: List<String> = emptyList(),
         val overview: String = "",
         val tel: String = "",
         val homepage: String = "",
-        val reviewWithWriterListUiState: ReviewWithWriterListUiState,
-        val ratingFilterOption: RatingFilterOption,
+        val ratingFilterOption: RatingFilterOption = RatingFilterOption.ALL,
     ) : ContentDetailScreenUiState
 }
