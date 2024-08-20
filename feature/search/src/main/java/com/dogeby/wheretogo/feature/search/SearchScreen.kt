6fad259b95c8a5ceacb55b3354e6fc5455bf03fc
dogeby <dogeby@gmail.com>
2024-08-21 00:38:35 +0900

    SearchScreen 색, tonalElevation 수정
    #161

--- feature/search/src/main/java/com/dogeby/wheretogo/feature/search/SearchScreen.kt
+++ feature/search/src/main/java/com/dogeby/wheretogo/feature/search/SearchScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.dogeby.wheretogo.core.ui.components.listitem.KeywordListItem
@@ -85,6 +88,10 @@ internal fun SearchScreen(
                     )
                 }
             },
+            colors = SearchBarDefaults.colors(
+                containerColor = MaterialTheme.colorScheme.background,
+            ),
+            tonalElevation = 0.dp,
         ) {
             LazyColumn {
                 items(recentQueries()) {
