ac49b17b0f13696f6bf6908acbdd04903c2aa19d
dogeby <dogeby@gmail.com>
2024-08-22 22:24:35 +0900

    RegionSelection Navigation, Route 구현
    #168

--- feature/regionselection/src/main/java/com/dogeby/wheretogo/feature/regionselection/navigation/RegionSelectionNavigation.kt
+++ feature/regionselection/src/main/java/com/dogeby/wheretogo/feature/regionselection/navigation/RegionSelectionNavigation.kt
@@ -1,12 +1,42 @@
 package com.dogeby.wheretogo.feature.regionselection.navigation
 
+import android.net.Uri
 import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.dogeby.wheretogo.core.common.decoder.StringDecoder
+import com.dogeby.wheretogo.feature.regionselection.RegionSelectionRoute
 
 internal const val CONTENT_TYPE_ID_ARG = "content_type_id"
+const val REGION_SELECTION_ROUTE = "region_selection_route/{$CONTENT_TYPE_ID_ARG}"
 
 internal class RegionSelectionArgs(val contentTypeId: String) {
 
     constructor(savedStateHandle: SavedStateHandle, stringDecoder: StringDecoder) :
         this(stringDecoder.decodeString(checkNotNull(savedStateHandle[CONTENT_TYPE_ID_ARG])))
+}
+
+fun NavController.navigateToRegionSelection(
+    contentTypeId: String,
+    navOptions: NavOptions? = null,
+) {
+    val encodedId = Uri.encode(contentTypeId)
+    this.navigate("region_selection_route/$encodedId", navOptions)
+}
+
+fun NavGraphBuilder.regionSelectionScreen(
+    onNavigateToList: (contentTypeId: String, areaCode: String) -> Unit,
+) {
+    composable(
+        route = REGION_SELECTION_ROUTE,
+        arguments = listOf(
+            navArgument(CONTENT_TYPE_ID_ARG) { type = NavType.StringType },
+        ),
+    ) {
+        RegionSelectionRoute(onNavigateToList)
+    }
 }
